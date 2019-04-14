ccc6dc29858e0c7e454f082c6f88c915aac49383
David L. Inman <davidlevere@gmail.com>
2019-04-13 20:48:32 -0400

    setting up better testing main environment

--- main.ml
+++ main.ml
@@ -3,17 +3,23 @@ open Lexer
 open Data
 
 let value = lex (object
-        val mutable x = true
+        val mutable _index = -1
+        val stuff = {|blah123_|} 
         method move_next = 
-            if x then (x <- false ; true)
-            else false
-        method current = '*'
-        method index = 0
+            if _index < String.length stuff - 1 then begin
+                _index <- _index + 1
+                ; true
+                end
+            else 
+                false
+        method current = stuff.[_index]  
+        method index = _index
      end
      );;
 
 let p = function
     | Number (x,_) -> "number"
+    | Symbol (s,i) -> Printf.sprintf "symbol %s %d" s i
     | Op (o,i) -> Printf.sprintf "op %s %d" o i
     | _ -> "empty" ;;
 
