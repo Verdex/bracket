bca7210e0050e08b7e6663e48204da753655fe21
David L. Inman <davidlevere@gmail.com>
2019-04-13 20:22:18 -0400

    adding printer stuff for token type

--- main.ml
+++ main.ml
@@ -7,12 +7,14 @@ let value = lex (object
         method move_next = 
             if x then (x <- false ; true)
             else false
-        method current = '5'
+        method current = '*'
+        method index = 0
      end
      );;
 
 let p = function
-    | Number (x,_) -> "blah"
+    | Number (x,_) -> "number"
+    | Op (o,i) -> Printf.sprintf "op %s %d" o i
     | _ -> "empty" ;;
 
 List.iter (fun x -> Printf.printf "%s\n" (p x)) value
