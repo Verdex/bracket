4918d90138aab7df612689bb96d657b0489da851
David L. Inman <davidlevere@gmail.com>
2019-04-13 20:19:10 -0400

    op lexer

--- lexer.ml
+++ lexer.ml
@@ -1,16 +1,34 @@
 
+open List
 open Data
+open Str
+
+let is_op c = 
+    match c with 
+    | '+' | '*' | '-' | '/' | '^'  
+    | '|' | '&' | '>' | '<' | '=' 
+    | '.' | '#' | '@' | '!' | '%' 
+    | '$' | '?' | '~' | '`' | ':' -> true
+    | _ -> false
+
+let lex_op input = 
+    let start = input#index in
+    let ret = Buffer.create 4 in
+    Buffer.add_char ret input#current
+    ; while input#move_next && is_op input#current do
+        Buffer.add_char ret input#current
+    done
+    ; Op (Buffer.contents ret, start)
 
 let lex (input : <current : char
+                 ;index : int
                  ;move_next : bool
                  >) =
     let ret = ref [] in 
     while input#move_next do
-        ret := List.cons (Number (5.0, ())) !ret 
+        match input#current with
+        | c when is_op c -> ret := lex_op input :: !ret
     done
-    ; !ret
-
-
-
+    ; rev !ret
 
 
--- lexer.mli
+++ lexer.mli
@@ -1,5 +1,7 @@
 
 open Data
 
-val lex : <current : char; move_next : bool> -> token list
+val lex : <current : char
+          ;index : int
+          ;move_next : bool> -> token list
 
